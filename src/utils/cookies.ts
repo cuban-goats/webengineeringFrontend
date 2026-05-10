fac9d6cf6fa873d7bbf7690ab38780d4b67a171a
Liam Wittig <liam.wittig@sap.com>
2026-05-10 17:35:07 +0200

    Add cookies max date

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -2,8 +2,9 @@ export function getCookie(name: string): string | undefined {
   return document.cookie.split("; ").find((c) => c.startsWith(name + "="))?.split("=")[1];
 }
 
-export function setCookie(name: string, value: string) {
-  document.cookie = `${name}=${value}; path=/`;
+export function setCookie(name: string, value: string, days = 30) {
+  const maxAge = days * 24 * 60 * 60;
+  document.cookie = `${name}=${value}; path=/; max-age=${maxAge}`;
 }
 
 export function deleteCookie(name: string) {
